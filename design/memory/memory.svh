ffcbb1d2d067bc2da58cb76872b4ece23d50a126
KangJongHyeon <rkdwhdgus09@gmail.com>
2026-06-06 17:28:30 +0900

    Update since ADDR_BIT

---
+++ design/memory/memory.svh
@@ -0,0 +1,8 @@
+///////////////////////////////////////
+// Path : .\design\memory\memory.svh //
+///////////////////////////////////////
+`timescale 1ns / 1ps
+// DRAM
+`define DRAM_SIZE 4096  // [Byte]
+// SRAM
+`define SRAM_SIZE 32768 // [Byte]
