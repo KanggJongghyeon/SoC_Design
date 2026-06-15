22877cd66c10c10c5e11bd9152bcb2b680a0cdcd
KangJongHyeon <rkdwhdgus09@gmail.com>
2026-06-15 21:05:33 +0900

    mkdir common and cp common .sv files

---
+++ design/common/adder.sv
@@ -0,0 +1,12 @@
+`timescale 1ns/1ps
+module adder21 #(
+    parameter DATA_BIT = 8
+    )(
+        input   wire    [DATA_BIT - 1:0]    i_i0,
+        input   wire    [DATA_BIT - 1:0]    i_i1,
+        output  wire    [DATA_BIT - 1:0]    o_o
+    );
+
+    assign o_o = i_i0 + i_i1;
+
+endmodule
