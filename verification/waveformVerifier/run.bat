e6bf86bb7b3c6267b01195213179b6ee7d49ab78
KangJongHyeon <rkdwhdgus09@gmail.com>
2026-08-27 22:23:28 +0900

    Auto Copy Mem File

--- verification/waveformVerifier/run.bat
+++ verification/waveformVerifier/run.bat
@@ -1,3 +1,15 @@
 @echo off
+
+REM Change Current Directory to the Batch File's Path
+cd /d "%~dp0"
+
+REM Copy Memory File
+REM /Y : Overwrite
+copy /Y .\..\..\tb\application.mem           .\memFile\application.mem
+copy /Y .\..\..\tb\boot_loader.mem           .\memFile\boot_loader.mem
+copy /Y .\..\..\tb\for_debug\debug_mode.mem  .\memFile\debug_mode.mem
+
+REM Run Python
 python main.py
+
 if errorlevel 1 pause
